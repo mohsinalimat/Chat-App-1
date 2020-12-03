f4666ed00053f23df9f1a12658416b15a48ac0fc
curdy <rhulani.ndhlvou1995@gmail.com>
2020-12-03 08:31:46 +0200

    Initial Commit

---
+++ Chat-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat-App
+//
+//  Created by Rhulani Ndhlovu on 2020/12/03.
+//  Copyright © 2020 Codenamerhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Chat-AppTests/Chat_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Chat_AppTests.swift
+//  Chat-AppTests
+//
+//  Created by Rhulani Ndhlovu on 2020/12/03.
+//  Copyright © 2020 Codenamerhu. All rights reserved.
+//
+
+import XCTest
+@testable import Chat_App
+
+class Chat_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
