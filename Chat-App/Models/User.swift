63489c8a1980f89a62dd4a4db265a2c64cf73d3c
curdy <rhulani.ndhlvou1995@gmail.com>
2020-12-03 09:22:35 +0200

    user model

---
+++ Chat-App/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Chat-App
+//
+//  Created by Rhulani Ndhlovu on 2020/12/03.
+//  Copyright © 2020 Codenamerhu. All rights reserved.
+//
+
+import Foundation
+
+class User: NSObject {
+    var phone : String?
+    var name : String?
+    var profileImageUrl : String?
+    var id : String?
+}
